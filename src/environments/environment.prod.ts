58daa76af7a0e6fee4f8b80b99a01caecfa9d3fd
Rodrigo Santucci <rodrigo_santucci@hotmail.com>
2025-05-22 20:43:13 -0300

    Ajusta build para produção acessando proxy-rev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { apiInterceptor } from "@core";
 
 export const environment = {
   production: true,
-  baseUrl: 'https://137.131.186.39:8443/',
-  authUrl: 'https://137.131.186.39:8443/',
+  baseUrl: 'https://apis.dividendosparatodos.com.br/services/',
+  authUrl: 'https://apis.dividendosparatodos.com.br/services/',
   useHash: false,
 };
