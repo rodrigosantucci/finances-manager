3ab6f2b2d049eb3e122eb8f9ed5b7c717a90f50a
Rodrigo Santucci <rodrigo_santucci@hotmail.com>
2026-02-13 16:34:20 -0300

    remove app android capacitor

--- android/app/src/main/java/com/dpt/financesmanager/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.dpt.financesmanager;
-
-import com.getcapacitor.BridgeActivity;
-
-
-public class MainActivity extends BridgeActivity {
-    // This class is intentionally empty. It is used to extend the BridgeActivity
-    // and can be customized further if needed.
-}
